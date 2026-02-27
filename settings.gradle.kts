94d9f30aad9eb579aa86926ca9cd897013f63293
int314 <77012577+314systems@users.noreply.github.com>
2026-02-27 22:42:34 +0900

    cleanup Gradle build files

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,5 +15,29 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>
  */
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
 
-include ':app'
+rootProject.name = "WiFiAnalyzer"
+include(":app")
